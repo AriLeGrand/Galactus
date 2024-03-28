a31f3ea8032a203f5c441bb5c231799386c5547b
AriLeGrand <arcgtroll.exe@gmail.com>
2024-03-28 21:43:04 +0100

    Co-authored-by: Raytroll <Raytroll@users.noreply.github.com>

--- BULLET.h
+++ BULLET.h
@@ -1,28 +1,30 @@
-#define MAX_BULLET_COUNT 100
+#define MAX_BULLET_COUNT 100;
 
 typedef struct BULLET {
     Vec2 pos;
     Vec2 vel;
     COLOR color;
     int opacity;
+    int size;
+    int ENABLED;
 } BULLET;
 
 BULLET init_bullet(Vec2 pos, Vec2 vel) {
-    return (BULLET) {pos, vel, (COLOR) {255, 255, 255}, 255};
+    return (BULLET) {pos, vel, (COLOR) {255, 255, 255}, 5, 255, 1};
 }
 
 void UPDATE_BULLETS(BULLET* BULLETS) {
     for (int i = 0; i < MAX_BULLET_COUNT; i++) {
+        if (BULLETS[i].ENABLED == 0) continue;
+        if (BULLETS[i].pos.x < 0 || BULLETS[i].pos.x > SW || BULLETS[i].pos.y < 0 || BULLETS[i].pos.y > SH) BULLETS[i].ENABLED = 1;
         BULLETS[i].pos.x += BULLETS[i].vel.x;
         BULLETS[i].pos.y += BULLETS[i].vel.y;
     }
 }
 
 void DISPLAY_BULLETS(BULLET* BULLETS, render_info* r_i) {
-    
     for (int i = 0; i < MAX_BULLET_COUNT; i++) {
-        // printf("%d\n", BULLETS[i].pos.x);
-        SDL_Rect sq = { BULLETS[i].pos.x, BULLETS[i].pos.y, 5, 5};
+        SDL_Rect sq = { BULLETS[i].pos.x, BULLETS[i].pos.y, BULLETS[i].size, BULLETS[i].size};
 
         SDL_SetRenderDrawColor(r_i->renderer, BULLETS[i].color.r, BULLETS[i].color.g, BULLETS[i].color.b, BULLETS[i].opacity);
         SDL_RenderFillRect(r_i->renderer, &sq);
